cc7a1425a01214465e106975e6690794d8551bdb
Aiden McClelland <me@drbonez.dev>
2024-03-07 17:03:09 -0700

    fix onion deserialization

--- src/onion/v3/serde_key.rs
+++ src/onion/v3/serde_key.rs
@@ -1,56 +1,87 @@
-use std::borrow::Cow;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::onion::v3::{TorPublicKeyV3, TorSecretKeyV3};
 
 impl Serialize for TorSecretKeyV3 {
-    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error> where
-        S: Serializer {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where
+        S: Serializer,
+    {
         serializer.serialize_str(&base64::encode(&self.as_bytes()[..]))
     }
 }
 
 impl Serialize for TorPublicKeyV3 {
-    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error> where
-        S: Serializer {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where
+        S: Serializer,
+    {
         serializer.serialize_str(&base64::encode(&self.0[..]))
     }
 }
 
 impl<'de> Deserialize<'de> for TorSecretKeyV3 {
-    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error> where
-        D: Deserializer<'de> {
-        let text = <Cow<'_, str>>::deserialize(deserializer)?;
-        let raw = base64::decode(&text[..]).map_err(serde::de::Error::custom)?;
-        if raw.len() != 64 {
-            return Err(serde::de::Error::custom("Invalid secret key length"));
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Visitor;
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = TorSecretKeyV3;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(formatter, "a raw onion address (without the .onion)")
+            }
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let raw = base64::decode(&v[..]).map_err(serde::de::Error::custom)?;
+                if raw.len() != 64 {
+                    return Err(serde::de::Error::custom("Invalid secret key length"));
+                }
+                let mut buf = [0u8; 64];
+                buf.clone_from_slice(&raw[..]);
+                Ok(Self::Value::from(buf))
+            }
         }
-        let mut buf = [0u8; 64];
-        buf.clone_from_slice(&raw[..]);
-        Ok(Self::from(buf))
+        deserializer.deserialize_str(Visitor)
     }
 }
 
 impl<'de> Deserialize<'de> for TorPublicKeyV3 {
-    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error> where
-        D: Deserializer<'de> {
-        let text = <Cow<'_, str>>::deserialize(deserializer)?;
-        let raw = base64::decode(&text[..]).map_err(serde::de::Error::custom)?;
-        if raw.len() != 32 {
-            return Err(serde::de::Error::custom("Invalid secret key length"));
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Visitor;
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = TorPublicKeyV3;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(formatter, "a raw onion address (without the .onion)")
+            }
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let raw = base64::decode(&v[..]).map_err(serde::de::Error::custom)?;
+                if raw.len() != 32 {
+                    return Err(serde::de::Error::custom("Invalid secret key length"));
+                }
+                let mut buf = [0u8; 32];
+                for i in 0..32 {
+                    buf[i] = raw[i];
+                }
+                Ok(TorPublicKeyV3(buf))
+            }
         }
-        let mut buf = [0u8; 32];
-        for i in 0..32 {
-            buf[i] = raw[i];
-        }
-        Ok(Self(buf))
+        deserializer.deserialize_str(Visitor)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::io::Cursor;
     use super::*;
+    use std::io::Cursor;
 
     #[test]
     fn test_can_serialize_and_deserialize_secret_key() {
